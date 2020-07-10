c90b4f589620c55dd79a96521b069ce70f91b142
Valery Karpov <karpoff19@mail.ru>
2020-07-10 19:54:27 +0300

    add log

--- src/main/java/by/karpov/ITSPartnertask/service/LampServiceImpl.java
+++ src/main/java/by/karpov/ITSPartnertask/service/LampServiceImpl.java
@@ -2,10 +2,12 @@ package by.karpov.ITSPartnertask.service;
 
 import by.karpov.ITSPartnertask.database.entity.Lamp;
 import by.karpov.ITSPartnertask.database.repository.LampRepo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @Transactional
 public class LampServiceImpl implements LampService<Lamp> {
@@ -19,6 +21,7 @@ public class LampServiceImpl implements LampService<Lamp> {
 
     @Override
     public Lamp updateLampCondition(Lamp lamp, String condition) {
+        log.info("In RoomServiceImpl save{}{}", lamp, condition);
         lamp.setLampCondition(condition);
         return lampRepo.save(lamp);
     }
