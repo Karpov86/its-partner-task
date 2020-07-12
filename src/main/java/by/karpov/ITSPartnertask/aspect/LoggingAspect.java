74f2c424ed5d36829fc7337e77caa87f61bf1e40
Valery Karpov <karpoff19@mail.ru>
2020-07-12 11:38:47 +0300

    add pointcut and advice for LampServiceImpl

--- src/main/java/by/karpov/ITSPartnertask/aspect/LoggingAspect.java
+++ src/main/java/by/karpov/ITSPartnertask/aspect/LoggingAspect.java
@@ -1,5 +1,6 @@
 package by.karpov.ITSPartnertask.aspect;
 
+import by.karpov.ITSPartnertask.database.entity.Lamp;
 import by.karpov.ITSPartnertask.database.entity.Room;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
@@ -19,27 +20,44 @@ import java.util.stream.Collectors;
 public class LoggingAspect {
 
     @Pointcut("execution(* by.karpov.ITSPartnertask.service.RoomServiceImpl.getAll())")
-    public void callAtMyServiceGetAll() {
+    public void callAtMyRoomServiceGetAll() {
     }
 
     @Pointcut("execution(* by.karpov.ITSPartnertask.service.RoomServiceImpl.*(..))")
-    public void callAtMyServicePublic() {
+    public void callAtMyRoomServicePublic() {
     }
 
-    @Before("callAtMyServicePublic()")
-    public void beforeCallMethod(JoinPoint jp) {
+    @Pointcut("@annotation(AspectAnnotation)")
+    public void callAtMyLampServicePublic() {
+    }
+
+    @Before("callAtMyLampServicePublic()")
+    public void beforeCallLampServiceMethod(JoinPoint jp) {
         String args = Arrays.stream(jp.getArgs())
                 .map(Object::toString)
                 .collect(Collectors.joining(","));
-        log.info("before {}, args=[{}]", jp.getSignature().getName(), args);
+        log.info("before {}, args=[{}]", jp.getSignature(), args);
     }
 
-    @AfterReturning(pointcut = "callAtMyServicePublic()", returning = "result")
+    @AfterReturning(pointcut = "callAtMyLampServicePublic()", returning = "result")
+    public void afterReturningCallAt(JoinPoint jp, Lamp result) {
+        log.info("method name: {}, return value: {}", jp.getSignature().getName(), result);
+    }
+
+    @Before("callAtMyRoomServicePublic()")
+    public void beforeCallRoomServiceMethod(JoinPoint jp) {
+        String args = Arrays.stream(jp.getArgs())
+                .map(Object::toString)
+                .collect(Collectors.joining(","));
+        log.info("before {}, args=[{}]", jp.getSignature(), args);
+    }
+
+    @AfterReturning(pointcut = "callAtMyRoomServicePublic()", returning = "result")
     public void afterReturningCallAt(JoinPoint jp, Room result) {
         log.info("method name: {}, return value: {}", jp.getSignature().getName(), result);
     }
 
-    @AfterReturning(pointcut = "callAtMyServiceGetAll()", returning = "list")
+    @AfterReturning(pointcut = "callAtMyRoomServiceGetAll()", returning = "list")
     public void afterReturningCallAt(JoinPoint jp, List<Room> list) {
         log.info("method name: {}, list size: {}", jp.getSignature().getName(), list.size());
     }
