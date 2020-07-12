dcc3c381da5c633a78d15054b738e5a6afba5e77
Valery Karpov <karpoff19@mail.ru>
2020-07-12 11:36:27 +0300

    add @AspectAnnotation for LoggingAspect

--- src/main/java/by/karpov/ITSPartnertask/service/LampServiceImpl.java
+++ src/main/java/by/karpov/ITSPartnertask/service/LampServiceImpl.java
@@ -1,5 +1,6 @@
 package by.karpov.ITSPartnertask.service;
 
+import by.karpov.ITSPartnertask.aspect.AspectAnnotation;
 import by.karpov.ITSPartnertask.database.entity.Lamp;
 import by.karpov.ITSPartnertask.database.repository.LampRepo;
 import lombok.extern.slf4j.Slf4j;
@@ -7,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Slf4j
+//@Slf4j
 @Service
 @Transactional
 public class LampServiceImpl implements LampService<Lamp> {
@@ -20,8 +21,9 @@ public class LampServiceImpl implements LampService<Lamp> {
     }
 
     @Override
+    @AspectAnnotation
     public Lamp updateLampCondition(Lamp lamp, String condition) {
-        log.info("In RoomServiceImpl update{}{}", lamp, condition);
+        //log.info("In RoomServiceImpl update{}{}", lamp, condition);
         lamp.setLampCondition(condition);
         return lampRepo.save(lamp);
     }
