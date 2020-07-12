1a9730324acca836880a047cbc0bd33888aa8b93
Valery Karpov <karpoff19@mail.ru>
2020-07-12 11:39:19 +0300

    add unit test for LampServiceImpl

---
+++ src/test/java/by/karpov/ITSPartnertask/LampServiceTest.java
@@ -0,0 +1,35 @@
+package by.karpov.ITSPartnertask;
+
+import by.karpov.ITSPartnertask.database.entity.Lamp;
+import by.karpov.ITSPartnertask.service.LampServiceImpl;
+import by.karpov.ITSPartnertask.service.RoomServiceImpl;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Sql(scripts = "/populate_db_for_test.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+public class LampServiceTest {
+
+    @Autowired
+    private RoomServiceImpl roomService;
+
+    @Autowired
+    private LampServiceImpl lampService;
+
+    @Test
+    @Transactional
+    public void updateLampConditionTest() {
+        Lamp lamp = roomService.findById(1L).getLamp();
+        String newCondition = "on";
+        String exceptedCondition = "on";
+        lampService.updateLampCondition(lamp, newCondition);
+
+        Assert.assertNotNull(lamp);
+        Assert.assertEquals(exceptedCondition, lamp.getLampCondition());
+
+    }
+}
